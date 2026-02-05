61c38143c5336b4a3904e9b3e617f707fd6bd34e
Service App <serviceapp@gmail.com>
2026-02-05 21:52:26 +0530

    fix generate invoice

--- service/schema.py
+++ service/schema.py
@@ -702,8 +702,14 @@ class GenerateInvoice(graphene.Mutation):
             repair_request = RepairRequest.objects.get(request_id=request_id)
         except RepairRequest.DoesNotExist:
             raise GraphQLError("Repair request not found")
+        
+        estimation=(Estimation.objects.filter(repair_request=repair_request).order_by("-created_at").first())
 
-        if repair_request.status != "READY_TO_DELIVER":
+        if not estimation:
+            raise GraphQLError("Estimation not found for this repair request")
+
+
+        if estimation.status != "READY_TO_DELIVER":
             raise GraphQLError("Invoice can only be generated at delivery state")
         # Check estimation exists
         estimation = Estimation.objects.filter(repair_request=repair_request).first()
