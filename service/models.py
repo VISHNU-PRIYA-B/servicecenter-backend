44eb50a249d7698c59712ad920fa512adacd2463
Service App <serviceapp@gmail.com>
2026-02-07 15:43:52 +0530

    Fix invoice

--- service/models.py
+++ service/models.py
@@ -191,6 +191,7 @@ class Updaterepairstatus(models.Model):
 
 
 class Invoice(models.Model):
+    estimation=models.OneToOneField(Estimation,on_delete=models.CASCADE)
     invoice_number = models.CharField(max_length=50,unique=True,null=True,blank=True)
     repair_request = models.ForeignKey(RepairRequest, on_delete=models.CASCADE)
     total_amount = models.DecimalField(max_digits=10, decimal_places=2)
